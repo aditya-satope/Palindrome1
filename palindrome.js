88b4a4f4be48ee25698053da634a83efbaf56d7e
Aditya Satope <adityasatope@gmail.com>
2022-11-06 13:33:09 +0530

    1.2
    Solving prompt error.

--- palindrome.js
+++ palindrome.js
@@ -1,4 +1,5 @@
 #! /usr/bin/env node
+const prompt = require("prompt-sync")({ sigint: true });
 let inp = prompt("enter word to check for palindrome");
 let test = inp.toLowerCase();
 let arr = test.split("");
