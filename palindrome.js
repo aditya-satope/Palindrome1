6717537e152f0db62d71a176f558ece58af8229f
Aditya Satope <adityasatope@gmail.com>
2022-11-06 13:11:42 +0530

    Update palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -1,14 +1,9 @@
 #! /usr/bin/env node
 let inp = prompt("enter word to check for palindrome");
-console.log(inp);
 let test = inp.toLowerCase();
-let rev = "";
 let arr = test.split("");
-console.log(arr);
 let arr1 = arr.reverse();
-console.log(arr1);
 let newstr = arr1.join("");
-console.log(newstr);
 if (newstr == test) {
   console.log("Yes it is a palindrome.");
 } else {
